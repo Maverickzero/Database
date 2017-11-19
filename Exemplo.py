e7ff6ecf91f249f8a95819538ae10d052382f956
YuriMF <yurifalcon7@gmail.com>
2017-11-19 16:41:48 +0100

    Now it runs

--- Exemplo.py
+++ Exemplo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-def MinhaFuncao(unput1,input2:
-    print('Hello %s and %s!') # Como voce pode ver por aqui a linha print comeca 4 espacos depois do def
+def MinhaFuncao(input1,input2):
+    print('Hello %s and %s!'%(input1,input2)) # Como voce pode ver por aqui a linha print comeca 4 espacos depois do def
 
 MinhaFuncao('Teste','Teste2')
