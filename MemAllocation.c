c02d0e92b270854b993f493166a8a18bcae5ac1e
YuriMF <yurifalcon7@gmail.com>
2017-11-30 17:40:25 +0100

    Now freeing memory when finished using it

--- MemAllocation.c
+++ MemAllocation.c
@@ -5,8 +5,8 @@ int main()
 {
   int *numeros, quantidade; // A gente ainda n sabe quandos numeros vao ter, entao em vez de comecar com um vetor, a gente comeca com um pointer
 
-  printf("Quantos numeros vc quer preencher?\n");
-  scanf("%d", &quantidade);
+  printf("Quantos numeros vc quer preencher? ");
+  scanf("%d", &quantidade); // Preenche a quantidade de numeros que o vetor deve ter
 
   numeros = (int*)malloc(quantidade*sizeof(int));
 
@@ -15,11 +15,12 @@ int main()
     printf("Entre o valor do numero %d: ", i+1);
     scanf("%d",&numeros[i]);
   }
-
   for(int i = 0; i < quantidade; i++)
   {
     printf("O valor do numero %d é %d.\n", i+1, numeros[i]);
   }
+
+  free(numeros);
 
   return 1;
 }
