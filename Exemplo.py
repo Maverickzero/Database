a7c6907847fc1b2719c948f26c3da8b31144f51d
YuriMF <yurifalcon7@gmail.com>
2017-12-02 21:23:45 +0100

    Changing example to match cpp database program, but in python

--- Exemplo.py
+++ Exemplo.py
@@ -1,5 +1,37 @@
-#!/usr/bin/env python
-def MinhaFuncao(input1,input2):
-    print('Hello %s and %s!'%(input1,input2)) # Como voce pode ver por aqui a linha print comeca 4 espacos depois do def
+#!/usr/bin/env python3
+Matriz = []
+for i in range(1,6):
+    conta = []
+    conta.append(int(input('Informe o numero da conta %d: '%i)))
+    conta.append(int(input('Informe o saldo da conta %d: '%i)))
+    Matriz.append(conta)
 
-MinhaFuncao('Teste','Teste2')
+codigo = None
+
+while(codigo != '5'):
+    codigo = input('\tFaca uma escolha entre as seguintes opcoes:\n\
+                   1. Efetuar um deposito em uma conta \n\
+                   2. Efetuar um saque de uma conta \n\
+                   3. Consultar o saldo de uma conta \n\
+                   4. Consultar o saldo em geral \n\
+                   5. Finalizar o programa\n')
+    if codigo == '1':
+        nr_conta = input('Digite o numero da conta: ')
+        for i in range(1, len(Matriz)+1):
+            conta = Matriz.pop()
+            if nr_conta == conta[0]:
+                deposito = int(input('Digite o valor do deposito: '))
+                conta[1] = conta[1] + deposito
+                Matriz.append(conta)
+        # do something
+#    elif codigo == '2':
+#        # do something
+#    elif codigo == '3':
+#        # do something
+#    elif codigo == '4':
+#        # do something
+#    elif codigo == '5':
+#        # do something
+
+for conta in Matriz:
+    print('A conta %d tem saldo %d'%(conta[0],conta[1]))
