7e5a10f336f00e8d790fef87312102e45d9d1e9a
YuriMF <yurifalcon7@gmail.com>
2017-12-10 12:07:03 +0100

    And now it is pep8-ified!

--- Exemplo.py
+++ Exemplo.py
@@ -27,10 +27,11 @@ while(codigo != '5'):
         nr_conta = input('Digite o numero da conta: ')
         if nr_conta in Matriz:
             try:
-                Matriz[nr_conta] = Matriz[nr_conta] + int(input('Digite a quantidade do depósito: '))
+                Matriz[nr_conta] = (Matriz[nr_conta]
+                    + int(input('Digite a quantidade do depósito: ')))
             except ValueError:
                 print('Erro conferindo o valor, você digitou numeros?')
-            print('O novo saldo da conta é %d'%Matriz[nr_conta])
+            print('O novo saldo da conta é %d' % Matriz[nr_conta])
         else:
             print('Conta inexistente, tente de novo.')
     elif codigo == '2':
@@ -44,17 +45,18 @@ while(codigo != '5'):
                     print('Conta nao tem saldo o suficiente para o saque.')
             except ValueError:
                 print('Erro conferindo o valor, você digitou numeros?')
-            print('O novo saldo da conta é %d'%Matriz[nr_conta])
+            print('O novo saldo da conta é %d' % Matriz[nr_conta])
         else:
             print('Conta inexistente, tente de novo.')
     elif codigo == '3':
         nr_conta = input('Digite o numero da conta: ')
         if nr_conta in Matriz:
-            print('O saldo da conta %s é %d.'%(nr_conta, Matriz[nr_conta]))
+            print('O saldo da conta %s é %d.' % (nr_conta, Matriz[nr_conta]))
         else:
             print('Conta inexistente, tente de novo.')
     elif codigo == '4':
         total = 0
         for conta in Matriz:
             total = total + Matriz[conta]
-        print('O saldo geral é %d.'%total)
+        print('O saldo geral é %d.' % total)
+
