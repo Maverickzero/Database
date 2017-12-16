df4ec7701e0d65134f5666c7bf5df452d7ee7469
YuriMF <yurifalcon7@gmail.com>
2017-12-16 18:46:44 +0100

    small changes

--- MemAllocation.c
+++ MemAllocation.c
@@ -3,12 +3,16 @@
 
 int main()
 {
-  int *numeros, quantidade; // A gente ainda n sabe quandos numeros vao ter, entao em vez de comecar com um vetor, a gente comeca com um pointer
+  int *numeros, quantidade, quant_calc; // A gente ainda n sabe quandos numeros vao ter, entao em vez de comecar com um vetor, a gente comeca com um pointer
 
   printf("Quantos numeros vc quer preencher? ");
   scanf("%d", &quantidade); // Preenche a quantidade de numeros que o vetor deve ter
 
   numeros = (int*)malloc(quantidade*sizeof(int));
+  quant_calc = sizeof(numeros)/sizeof(int);
+  printf("Pointer aponta gora para um bloco de memoria de %ld bytes. "
+         "Com um vetor de %d dimensoes.\n", 
+         sizeof(numeros), quant_calc);
 
   for(int i = 0; i < quantidade; i++)
   {
