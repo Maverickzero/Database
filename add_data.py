652f553aa129a427713ad185064d494b616443d2
YuriMF <yurifalcon7@gmail.com>
2018-01-20 11:25:55 +0100

    and now it is using mariaDB/mySQL (need to install mariaDB first to make it work!)

--- add_data.py
+++ add_data.py
@@ -5,39 +5,44 @@ import peewee
 
 from models import Album, Artist
 
-new_artist = Artist.create(name="Newsboys", address=None)
-album_one = Album(artist=new_artist,
-                  title="Read All About It",
-                  release_date=datetime.date(1988,12,1),
-                  publisher="Refuge",
-                  media_type="CD")
+art = Artist()
+alb = Album()
+new_artist = art.get_or_create(name = "Newsboys", address = None)
+album_one = alb.get_or_create(artist = new_artist,
+                              title = "Read All About It",
+                              release_date = datetime.date(1988,12,1),
+                              publisher = "Refuge",
+                              media_type = "CD")
 album_one.save()
 
-albums = [{"artist": new_artist,
+albums = [{
+           "artist": new_artist,
            "title": "Hell is for Wimps",
            "release_date": datetime.date(1990,7,31),
            "publisher": "Sparrow",
            "media_type": "CD"
-           },
-          {"artist": new_artist,
+          },
+          {
+           "artist": new_artist,
            "title": "Love Liberty Disco",
            "release_date": datetime.date(1999,11,16),
            "publisher": "Sparrow",
            "media_type": "CD"
           },
-          {"artist": new_artist,
+          {
+           "artist": new_artist,
            "title": "Thrive",
            "release_date": datetime.date(2002,3,26),
            "publisher": "Sparrow",
-           "media_type": "CD"}
-          ]
+           "media_type": "CD"
+          }]
 
 for album in albums:
-    a = Album(**album)
+    a = alb.get_or_create(**album)
     a.save()
 
 bands = ["MXPX", "Kutless", "Thousand Foot Krutch"]
 for band in bands:
-    artist = Artist.create(name=band, address=None)
+    artist = art.get_or_create(name=band, address=None)
     artist.save()
 
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import peewee
+import pymysql
 
-database = peewee.SqliteDatabase("wee.db")
+#database = peewee.SqliteDatabase("wee.db")
+database = peewee.MySQLDatabase('testDB', password = '@l3ks@ndri@', user = 'jh0wlett')
 
 ########################################################################
 class Artist(peewee.Model):
@@ -9,7 +11,12 @@ class Artist(peewee.Model):
     """
     name = peewee.CharField(primary_key = True)
     address = peewee.CharField(null = True)
-
+    def get_or_create(self, name, address):
+        try:
+            new_artist = Artist.get(Artist.name == name)
+        except:
+            new_artist = self.create(name = name, address = address)
+        return new_artist
     class Meta:
         database = database
 
@@ -23,17 +30,26 @@ class Album(peewee.Model):
     release_date = peewee.DateTimeField()
     publisher = peewee.CharField()
     media_type = peewee.CharField()
-
+    def get_or_create(self, artist, title, release_date, publisher, media_type):
+        try:
+            new_album = Album.get(Album.title == title)
+        except:
+            new_album =  self.create(artist = artist,
+                                     title = title,
+                                     release_date = release_date,
+                                     publisher = publisher,
+                                     media_type = media_type)
+        return new_album
     class Meta:
         database = database
 
 try:
     Artist.create_table()
-except peewee.OperationalError:
+except (peewee.OperationalError, peewee.InternalError):
     print("Artist table already exists!")
 
 try:
     Album.create_table()
-except peewee.OperationalError:
+except (peewee.OperationalError, peewee.InternalError):
     print("Album table already exists!")
 
